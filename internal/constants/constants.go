345e192ba09dcf5e5cef152990d2f58c10cc2b36
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-07-23 21:31:25 +0100

    Update Microsoft Graph API service roots for different clouds

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -2,24 +2,32 @@ package constants
 
 const (
 	// Public Cloud
-	PUBLIC_OAUTH_AUTHORITY_URL = "https://login.microsoftonline.com/"
-	PUBLIC_GRAPH_API_SCOPE     = "https://graph.microsoft.com/.default"
+	PUBLIC_OAUTH_AUTHORITY_URL         = "https://login.microsoftonline.com/"
+	PUBLIC_GRAPH_API_SCOPE             = "https://graph.microsoft.com/.default"
+	PUBLIC_GRAPH_API_SERVICE_ROOT      = "https://graph.microsoft.com/v1.0"
+	PUBLIC_GRAPH_BETA_API_SERVICE_ROOT = "https://graph.microsoft.com/beta"
 
 	// US Department of Defense (DoD) Cloud
-	USDOD_OAUTH_AUTHORITY_URL = "https://login.microsoftonline.us/"
-	USDOD_GRAPH_API_SCOPE     = "https://graph.microsoft.us/.default"
+	USDOD_OAUTH_AUTHORITY_URL         = "https://login.microsoftonline.us/"
+	USDOD_GRAPH_API_SCOPE             = "https://graph.microsoft.us/.default"
+	USDOD_GRAPH_API_SERVICE_ROOT      = "https://dod-graph.microsoft.us/v1.0"
+	USDOD_GRAPH_BETA_API_SERVICE_ROOT = "https://dod-graph.microsoft.us/beta"
 
 	// US Government Cloud
-	USGOV_OAUTH_AUTHORITY_URL = "https://login.microsoftonline.com/"
-	USGOV_GRAPH_API_SCOPE     = "https://graph.microsoft.us/.default"
+	USGOV_OAUTH_AUTHORITY_URL         = "https://login.microsoftonline.com/"
+	USGOV_GRAPH_API_SCOPE             = "https://graph.microsoft.us/.default"
+	USGOV_GRAPH_API_SERVICE_ROOT      = "https://graph.microsoft.us/v1.0"
+	USGOV_GRAPH_BETA_API_SERVICE_ROOT = "https://graph.microsoft.us/beta"
 
 	// US Government High Cloud
 	USGOVHIGH_OAUTH_AUTHORITY_URL = "https://login.microsoftonline.us/"
 	USGOVHIGH_GRAPH_API_SCOPE     = "https://graph.microsoft.us/.default"
 
 	// China Cloud - https://learn.microsoft.com/en-us/previous-versions/office/office-365-api/api/o365-china-endpoints
-	CHINA_OAUTH_AUTHORITY_URL = "https://login.chinacloudapi.cn/"
-	CHINA_GRAPH_API_SCOPE     = "https://microsoftgraph.chinacloudapi.cn/.default"
+	CHINA_OAUTH_AUTHORITY_URL         = "https://login.chinacloudapi.cn/"
+	CHINA_GRAPH_API_SCOPE             = "https://microsoftgraph.chinacloudapi.cn/.default"
+	CHINA_GRAPH_API_SERVICE_ROOT      = "https://microsoftgraph.chinacloudapi.cn/v1.0"
+	CHINA_GRAPH_BETA_API_SERVICE_ROOT = "https://microsoftgraph.chinacloudapi.cn/beta"
 
 	// EagleX Cloud
 	EX_OAUTH_AUTHORITY_URL = "https://login.microsoftonline.eaglex.ic.gov/"
