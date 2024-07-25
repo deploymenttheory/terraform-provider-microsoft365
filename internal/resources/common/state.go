1cfd7bb6d6df3eb228d135dac6fe0f82f5e53599
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-07-25 11:44:53 +0100

    refactor: Improve assignment filter resource construction
    Refactor the construction of the assignment filter resource in `state.go` and `object.go` for improved readability and visibility. The changes include:
    - Safely dereferencing string pointers to avoid nil pointer errors
    - Safely converting enums to their string representation
    - Handling nil values for various fields in the assignment filter resource
    
    These refactorings enhance the code readability and provide better visibility into the constructed assignment filter resource.

--- internal/resources/common/state.go
+++ internal/resources/common/state.go
@@ -1,56 +1,9 @@
 package common
 
-import (
-	"fmt"
-
-	"github.com/hashicorp/terraform-plugin-framework/types"
-
-	"github.com/hashicorp/terraform-plugin-framework/attr"
-)
-
-// SetStringValueFromAttributes sets a string value from the given attribute map if the key exists and is not null.
-// It takes a map of attributes, a key to look for, and a setter function that sets the value if found.
-func SetStringValueFromAttributes(attrs map[string]attr.Value, key string, setter func(*string)) {
-	if v, ok := attrs[key].(types.String); ok && !v.IsNull() {
-		str := v.ValueString()
-		setter(&str)
-	}
-}
-
-// SetParsedValueFromAttributes sets a parsed value from the given attribute map if the key exists and is not null.
-// It takes a map of attributes, a key to look for, a setter function to set the parsed value, and a parser function
-// to convert the string value to the desired type. It returns an error if parsing fails.
-func SetParsedValueFromAttributes[T any](attrs map[string]attr.Value, key string, setter func(*T), parser func(string) (interface{}, error)) error {
-	if v, ok := attrs[key].(types.String); ok && !v.IsNull() {
-		str := v.ValueString()
-		parsedValue, err := parser(str)
-		if err != nil {
-			return err
-		}
-		if parsedValue != nil {
-			setter(parsedValue.(*T))
-		}
-	}
-	return nil
-}
-
-// safeDeref safely dereferences a string pointer.
-// It returns an empty string if the pointer is nil,
-// otherwise it returns the dereferenced string value.
-func SafeDeref(s *string) string {
+// StringPtrToString converts a string pointer to a string.
+func StringPtrToString(s *string) string {
 	if s == nil {
 		return ""
 	}
 	return *s
-}
-
-// safeEnumString safely converts an enum to its string representation.
-// It returns an empty string if the enum is nil,
-// otherwise it calls the String() method on the enum.
-// This function expects the input to implement the fmt.Stringer interface.
-func SafeEnumString(e fmt.Stringer) string {
-	if e == nil {
-		return ""
-	}
-	return e.String()
 }
