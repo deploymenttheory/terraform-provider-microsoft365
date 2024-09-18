437826c78273f8ff21a65f9869d0d358ee74ade5
ShocOne <dafydd.watkins@deploymenttheory.com>
2024-09-18 12:41:04 +0100

    Refactor: Update MapRemoteStateToTerraform function to handle empty RoleScopeTagIds

--- internal/resources/device_and_app_management/beta/winget_app/state.go
+++ internal/resources/device_and_app_management/beta/winget_app/state.go
@@ -58,7 +58,7 @@ func MapRemoteStateToTerraform(ctx context.Context, data *WinGetAppResourceModel
 	// Handle RoleScopeTagIds
 	roleScopeTags := remoteResource.GetRoleScopeTagIds()
 
-	if roleScopeTags != nil && len(roleScopeTags) > 0 {
+	if len(roleScopeTags) > 0 {
 		data.RoleScopeTagIds = make([]types.String, len(roleScopeTags))
 		for i, tag := range roleScopeTags {
 			data.RoleScopeTagIds[i] = types.StringValue(tag)
