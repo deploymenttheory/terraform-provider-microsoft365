a595cd7ab55b573cad1192cd294eb7e5a07e86b7
ShocOne <dafydd.watkins@deploymenttheory.com>
2024-11-17 10:06:38 +0000

    Refactor: Enhance resource construction by adding resource type name for improved logging

--- internal/resources/_resource_template/construct.go
+++ internal/resources/_resource_template/construct.go
@@ -2,6 +2,7 @@ package graphBetaAssignmentFilter
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/deploymenttheory/terraform-provider-microsoft365/internal/resources/common/construct"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
@@ -10,8 +11,8 @@ import (
 )
 
 // constructResource maps the Terraform schema to the SDK model
-func constructResource(ctx context.Context, data *AssignmentFilterResourceModel) (*models.DeviceAndAppManagementAssignmentFilter, error) {
-	tflog.Debug(ctx, "Constructing 'resource_name' resource from model")
+func constructResource(ctx context.Context, typeName string, data *AssignmentFilterResourceModel) (*models.DeviceAndAppManagementAssignmentFilter, error) {
+	tflog.Debug(ctx, fmt.Sprintf("Constructing %s resource from model", typeName))
 
 	requestBody := models.NewDeviceAndAppManagementAssignmentFilter()
 
@@ -20,11 +21,13 @@ func constructResource(ctx context.Context, data *AssignmentFilterResourceModel)
 
 	// add more fields here as needed
 
-	if err := construct.DebugLogGraphObject(ctx, "Final JSON to be sent to Graph API", requestBody); err != nil {
+	if err := construct.DebugLogGraphObject(ctx, fmt.Sprintf("Final JSON to be sent to Graph API for resource %s", typeName), requestBody); err != nil {
 		tflog.Error(ctx, "Failed to debug log object", map[string]interface{}{
 			"error": err.Error(),
 		})
 	}
+
+	tflog.Debug(ctx, fmt.Sprintf("Finished constructing %s resource", typeName))
 
 	return requestBody, nil
 }
