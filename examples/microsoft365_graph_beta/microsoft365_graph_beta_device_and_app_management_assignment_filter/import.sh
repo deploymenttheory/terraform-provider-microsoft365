e21703edd10215739ebd47d55718e9c3cf44fdef
ShocOne <dafydd.watkins@deploymenttheory.com>
2024-11-19 05:58:03 +0000

    Add import scripts and templates for Intune Assignment Filter resource

---
+++ examples/microsoft365_graph_beta/microsoft365_graph_beta_device_and_app_management_assignment_filter/import.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Using the provider-default project ID, the import ID is:
+# {resource_id}
+terraform import microsoft365_graph_beta_device_and_app_management_assignment_filter.example assignment-filter-id
--- examples/microsoft365_graph_beta/microsoft365_graph_beta_device_and_app_management_device_management_script/import.sh
+++ examples/microsoft365_graph_beta/microsoft365_graph_beta_device_and_app_management_device_management_script/import.sh
@@ -1,7 +1,6 @@
-# Using an explicit project ID, the import ID is:
-# {project_id}:{cluster_id}
-terraform import microsoft365_graph_beta_device_and_app_management_device_management_script.example f709ec73-55d4-46d8-897d-816ebba28778:device-management-script
+#!/bin/bash
+
 # Using the provider-default project ID, the import ID is:
-# {cluster_id}
-terraform import microsoft365_graph_beta_device_and_app_management_device_management_script.example device-management-script
+# {resource_id}
+terraform terraform import microsoft365_graph_beta_device_and_app_management_device_management_script.example device-management-script-id
 
