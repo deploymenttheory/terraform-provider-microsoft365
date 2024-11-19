2cd458691e870533d82d8d8a9e48f4af81569503
ShocOne <dafydd.watkins@deploymenttheory.com>
2024-11-19 05:50:44 +0000

    Add import script and template for Intune Device Management Script resource

---
+++ examples/microsoft365_graph_beta/microsoft365_graph_beta_device_and_app_management_device_management_script/import.sh
@@ -0,0 +1,7 @@
+# Using an explicit project ID, the import ID is:
+# {project_id}:{cluster_id}
+terraform import microsoft365_graph_beta_device_and_app_management_device_management_script.example f709ec73-55d4-46d8-897d-816ebba28778:device-management-script
+# Using the provider-default project ID, the import ID is:
+# {cluster_id}
+terraform import microsoft365_graph_beta_device_and_app_management_device_management_script.example device-management-script
+
