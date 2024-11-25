5caf2bef54bbb5de6737305105052abf5e4067da
ShocOne <dafydd.watkins@deploymenttheory.com>
2024-11-25 17:51:11 +0000

    Add Device Shell Script resource and example configurations; include outputs and import script

---
+++ examples/microsoft365_graph_beta/microsoft365_graph_beta_device_and_app_management_device_shell_script/datasource.tf
@@ -0,0 +1,27 @@
+# Outputs
+output "new_script_id" {
+  value       = microsoft365_graph_beta_device_and_app_management_device_shell_script.example.id
+  description = "ID of the newly created Device Management Script"
+}
+
+output "existing_script_display_name" {
+  value       = data.microsoft365_graph_beta_device_and_app_management_device_shell_script.existing_script.display_name
+  description = "Display name of the existing Device Management Script"
+}
+
+output "existing_script_last_modified" {
+  value       = data.microsoft365_graph_beta_device_and_app_management_device_shell_script.existing_script.last_modified_date_time
+  description = "Last modified date of the existing Device Management Script"
+}
+
+output "existing_script_assignments" {
+  value       = data.microsoft365_graph_beta_device_and_app_management_device_shell_script.existing_script.assignments
+  description = "Assignments of the existing Device Management Script"
+  sensitive   = true
+}
+
+output "existing_script_group_assignments" {
+  value       = data.microsoft365_graph_beta_device_and_app_management_device_shell_script.existing_script.group_assignments
+  description = "Group assignments of the existing Device Management Script"
+  sensitive   = true
+}
---
+++ examples/microsoft365_graph_beta/microsoft365_graph_beta_device_and_app_management_device_shell_script/resource.tf
@@ -0,0 +1,41 @@
+// Example: Device Shell Script Resource
+
+resource "microsoft365_graph_beta_device_and_app_management_device_shell_script" "example" {
+  display_name       = "Example Device Shell Script 1"
+  description        = "This is an example device management script"
+  role_scope_tag_ids = ["0"]
+
+  # The actual script content (should be PowerShell script content only)
+  script_content = <<EOT
+    # Your PowerShell script here
+    Write-Host "Hello from device management script!"
+    # Add more PowerShell commands as needed
+  EOT
+
+  run_as_account         = "system" # Can be "system" or "user"
+  enforce_signature_check = false
+  file_name              = "example_script.ps1"
+  run_as_32_bit          = false
+
+  assignments = {
+    all_devices = false
+    all_users   = false
+
+    include_group_ids = [
+      "51a96cdd-4b9b-4849-b416-8c94a6d88797",
+      "b15228f4-9d49-41ed-9b4f-0e7c721fd9c2"
+    ]
+
+    exclude_group_ids = [
+      "b8c661c2-fa9a-4351-af86-adc1729c343f",
+      "f6ebd6ff-501e-4b3d-a00b-a2e102c3fa0f"
+    ]
+  }
+
+  timeouts = {
+    create = "1m"
+    read   = "1m"
+    update = "1m"
+    delete = "1m"
+  }
+}
