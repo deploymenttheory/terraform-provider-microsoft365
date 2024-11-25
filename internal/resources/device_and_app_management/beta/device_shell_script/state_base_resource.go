99d45d558120c49a295b0eae7bf5443610596cbb
ShocOne <dafydd.watkins@deploymenttheory.com>
2024-11-25 17:48:06 +0000

    Refactor MapRemoteResourceStateToTerraform function for clarity and improved error handling; update mapping of additional properties for DeviceShellScriptResourceModel

--- internal/resources/device_and_app_management/beta/device_shell_script/state_base_resource.go
+++ internal/resources/device_and_app_management/beta/device_shell_script/state_base_resource.go
@@ -1,8 +1,10 @@
+// MapRemoteResourceStateToTerraform states the base properties of a SettingsCatalogProfileResourceModel to a Terraform state
 package graphBetaDeviceShellScript
 
 import (
 	"context"
 	"encoding/base64"
+	"fmt"
 
 	"github.com/deploymenttheory/terraform-provider-microsoft365/internal/resources/common/state"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -10,8 +12,8 @@ import (
 	graphmodels "github.com/microsoftgraph/msgraph-beta-sdk-go/models"
 )
 
-// MapRemoteResourceStateToTerraform states the base properties of a SettingsCatalogProfileResourceModel to a Terraform state
-func MapRemoteResourceStateToTerraform(ctx context.Context, data *DeviceShellScriptResourceModel, remoteResource graphmodels.DeviceManagementScriptable) {
+// MapRemoteResourceStateToTerraform maps the base properties of a DeviceShellScriptResourceModel to a Terraform state.
+func MapRemoteResourceStateToTerraform(ctx context.Context, data *DeviceShellScriptResourceModel, remoteResource graphmodels.DeviceShellScriptable) {
 	if remoteResource == nil {
 		tflog.Debug(ctx, "Remote resource is nil")
 		return
@@ -24,22 +26,28 @@ func MapRemoteResourceStateToTerraform(ctx context.Context, data *DeviceShellScr
 	data.ID = types.StringValue(state.StringPtrToString(remoteResource.GetId()))
 	data.DisplayName = types.StringValue(state.StringPtrToString(remoteResource.GetDisplayName()))
 	data.Description = types.StringValue(state.StringPtrToString(remoteResource.GetDescription()))
-	decoded, err := base64.StdEncoding.DecodeString(string(remoteResource.GetScriptContent()))
+	data.CreatedDateTime = state.TimeToString(remoteResource.GetCreatedDateTime())
+	data.LastModifiedDateTime = state.TimeToString(remoteResource.GetLastModifiedDateTime())
+	data.RunAsAccount = state.EnumPtrToTypeString(remoteResource.GetRunAsAccount())
+	data.FileName = types.StringValue(state.StringPtrToString(remoteResource.GetFileName()))
+	data.RoleScopeTagIds = state.SliceToTypeStringSlice(remoteResource.GetRoleScopeTagIds())
+	data.BlockExecutionNotifications = types.BoolValue(state.BoolPtrToBool(remoteResource.GetBlockExecutionNotifications()))
+
+	// ExecutionFrequency (ISO Duration)
+	if executionFrequency := remoteResource.GetExecutionFrequency(); executionFrequency != nil {
+		data.ExecutionFrequency = types.StringValue(fmt.Sprintf("%v", executionFrequency.String()))
+	}
+
+	// ScriptContent (base64 encoded)
+	decodedContent, err := base64.StdEncoding.DecodeString(string(remoteResource.GetScriptContent()))
 	if err != nil {
 		tflog.Warn(ctx, "Failed to decode base64 script content", map[string]interface{}{
 			"error": err.Error(),
 		})
 		data.ScriptContent = types.StringValue(string(remoteResource.GetScriptContent())) // Use original if decode fails
-		return
+	} else {
+		data.ScriptContent = types.StringValue(string(decodedContent))
 	}
-	data.ScriptContent = types.StringValue(string(decoded))
-	data.CreatedDateTime = state.TimeToString(remoteResource.GetCreatedDateTime())
-	data.LastModifiedDateTime = state.TimeToString(remoteResource.GetLastModifiedDateTime())
-	data.RunAsAccount = state.EnumPtrToTypeString(remoteResource.GetRunAsAccount())
-	data.EnforceSignatureCheck = types.BoolValue(state.BoolPtrToBool(remoteResource.GetEnforceSignatureCheck()))
-	data.FileName = types.StringValue(state.StringPtrToString(remoteResource.GetFileName()))
-	data.RunAs32Bit = types.BoolValue(state.BoolPtrToBool(remoteResource.GetRunAs32Bit()))
-	data.RoleScopeTagIds = state.SliceToTypeStringSlice(remoteResource.GetRoleScopeTagIds())
 
 	tflog.Debug(ctx, "Finished mapping remote resource state to Terraform state", map[string]interface{}{
 		"resourceId": data.ID.ValueString(),
