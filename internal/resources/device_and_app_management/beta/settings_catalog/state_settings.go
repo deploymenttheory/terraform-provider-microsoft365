1b060e6a8e30ca6d84dd87578993730bee7294fc
ShocOne <dafydd.watkins@deploymenttheory.com>
2024-11-29 16:29:12 +0000

    Refactor settings retrieval to use custom GET request; update MapRemoteSettingsStateToTerraform to handle raw JSON response and normalize settings data.

--- internal/resources/device_and_app_management/beta/settings_catalog/state_settings.go
+++ internal/resources/device_and_app_management/beta/settings_catalog/state_settings.go
@@ -7,37 +7,22 @@ import (
 	"github.com/deploymenttheory/terraform-provider-microsoft365/internal/resources/common/normalize"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
-	graphmodels "github.com/microsoftgraph/msgraph-beta-sdk-go/models"
 )
 
 // MapRemoteSettingsStateToTerraform maps the remote settings catalog settings state to the Terraform state
 // using the shared DeviceConfigV2GraphServiceMode used also for requests.
 // Results are normalized and stored alphabetically in the Terraform state.
-func MapRemoteSettingsStateToTerraform(ctx context.Context, data *SettingsCatalogProfileResourceModel, remoteSettings []graphmodels.DeviceManagementConfigurationSettingable) {
-	if remoteSettings == nil {
-		tflog.Debug(ctx, "Remote settings are nil")
+func MapRemoteSettingsStateToTerraform(ctx context.Context, data *SettingsCatalogProfileResourceModel, resp []byte) {
+	if err := json.Unmarshal(resp, &DeviceConfigV2GraphServiceModel); err != nil {
+		tflog.Error(ctx, "Failed to unmarshal settings", map[string]interface{}{"error": err.Error()})
 		return
 	}
 
-	tflog.Debug(ctx, "Starting to map settings state to Terraform state")
-
-	jsonData, err := json.Marshal(&DeviceConfigV2GraphServiceModel)
+	normalizedJSON, err := normalize.JSONAlphabetically(string(resp))
 	if err != nil {
-		tflog.Error(ctx, "Failed to marshal settings data to JSON", map[string]interface{}{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	normalizedJSON, err := normalize.JSONAlphabetically(string(jsonData))
-	if err != nil {
-		tflog.Error(ctx, "Failed to normalize JSON", map[string]interface{}{
-			"error": err.Error(),
-		})
+		tflog.Error(ctx, "Failed to normalize JSON", map[string]interface{}{"error": err.Error()})
 		return
 	}
 
 	data.Settings = types.StringValue(normalizedJSON)
-
-	tflog.Debug(ctx, "Finished mapping settings state to Terraform state")
 }
