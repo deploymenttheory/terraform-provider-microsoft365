a086ad992a23606c52cf5d56c58315aa327e36a1
ShocOne <dafydd.watkins@deploymenttheory.com>
2025-01-03 12:03:59 +0000

    refactor: update import paths for device and app management resources to use graph_beta and graph_v1.0

--- internal/datasources/device_and_app_management/graph_beta/macos_platform_script/read.go
+++ internal/datasources/device_and_app_management/graph_beta/macos_platform_script/read.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/deploymenttheory/terraform-provider-microsoft365/internal/resources/common/crud"
-	resource "github.com/deploymenttheory/terraform-provider-microsoft365/internal/resources/device_and_app_management/beta/windows_platform_script"
+	resource "github.com/deploymenttheory/terraform-provider-microsoft365/internal/resources/device_and_app_management/graph_beta/windows_platform_script"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	graphmodels "github.com/microsoftgraph/msgraph-beta-sdk-go/models"
