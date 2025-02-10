cd0433be797cafa09d609a82a4f39654f95ed8d2
ShocOne <dafydd.watkins@deploymenttheory.com>
2025-02-10 16:22:03 +0000

    content upload builder factory refactor

--- internal/resources/device_and_app_management/graph_beta/applications/crud_content_upload.go
+++ internal/resources/device_and_app_management/graph_beta/applications/crud_content_upload.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/deploymenttheory/terraform-provider-microsoft365/internal/resources/common/errors"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/microsoftgraph/msgraph-beta-sdk-go/deviceappmanagement"
 	graphmodels "github.com/microsoftgraph/msgraph-beta-sdk-go/models"
 )
 
@@ -38,47 +40,112 @@ func initializeContentIfNeeded(ctx context.Context, r *ApplicationsResource, obj
 	return nil
 }
 
-// initializeContentUpload handles content initialization of application upload
 func (r *ApplicationsResource) initializeContentUpload(ctx context.Context, object *ApplicationsResourceModel) (graphmodels.MobileAppContentable, error) {
-	appType := object.ApplicationType.ValueString()
-
-	contentVersionsBuilder, err := r.NewContentVersionsBuilder(appType, object.ID.ValueString())
+	builder, err := r.NewContentVersionsBuilder(object.ApplicationType.ValueString(), object.ID.ValueString())
 	if err != nil {
-		return nil, fmt.Errorf("failed to get content versions builder: %w", err)
+		return nil, err
 	}
 
-	contentVersion, err := contentVersionsBuilder.(interface {
-		Post(ctx context.Context, body graphmodels.MobileAppContentable, config *ApplicationsResourceModel) (graphmodels.MobileAppContentable, error)
-	}).Post(ctx, graphmodels.NewMobileAppContent(), nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize content version for resource %s: %s", object.ID.ValueString(), err.Error())
-	}
+	content := graphmodels.NewMobileAppContent()
 
-	return contentVersion, nil
+	switch object.ApplicationType.ValueString() {
+	case "AndroidLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphAndroidLobAppContentVersionsRequestBuilder).Post(ctx, content, nil)
+	case "IosLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphIosLobAppContentVersionsRequestBuilder).Post(ctx, content, nil)
+	case "MacOSDmgApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphMacOSDmgAppContentVersionsRequestBuilder).Post(ctx, content, nil)
+	case "MacOSLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphMacOSLobAppContentVersionsRequestBuilder).Post(ctx, content, nil)
+	case "MacOSPkgApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphMacOSPkgAppContentVersionsRequestBuilder).Post(ctx, content, nil)
+	case "ManagedAndroidLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphManagedAndroidLobAppContentVersionsRequestBuilder).Post(ctx, content, nil)
+	case "ManagedIOSLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphManagedIOSLobAppContentVersionsRequestBuilder).Post(ctx, content, nil)
+	case "ManagedMobileLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphManagedMobileLobAppContentVersionsRequestBuilder).Post(ctx, content, nil)
+	case "Win32LobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphWin32LobAppContentVersionsRequestBuilder).Post(ctx, content, nil)
+	case "WindowsAppX":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphWindowsAppXContentVersionsRequestBuilder).Post(ctx, content, nil)
+	default:
+		return nil, fmt.Errorf("unsupported application type: %s", object.ApplicationType.ValueString())
+	}
 }
 
 // contentFileUpload handles the file upload for the application content
-func (r *ApplicationsResource) contentFileUpload(ctx context.Context, object *ApplicationsResourceModel, contentVersion graphmodels.MobileAppContentable) (string, error) {
-	appType := object.ApplicationType.ValueString()
-
-	filesBuilder, err := r.NewContentVersionsBuilder(appType, object.ID.ValueString())
+func (r *ApplicationsResource) contentFileUpload(ctx context.Context, object *ApplicationsResourceModel, contentVersion graphmodels.MobileAppContentable) (graphmodels.MobileAppContentFileable, error) {
+	builder, err := r.NewContentVersionsBuilder(object.ApplicationType.ValueString(), object.ID.ValueString())
 	if err != nil {
-		return "", fmt.Errorf("failed to get files builder: %w", err)
+		return nil, err
 	}
 
-	uploadUrl, err := filesBuilder.(interface {
-		ByMobileAppContentId(contentId string) interface {
-			Files() interface {
-				Post(ctx context.Context, body graphmodels.MobileAppContentFileable, config *ApplicationsResourceModel) (string, error)
-			}
-		}
-	}).ByMobileAppContentId(*contentVersion.GetId()).Files().Post(ctx, graphmodels.NewMobileAppContentFile(), nil)
+	contentFile := graphmodels.NewMobileAppContentFile()
+	fileName := filepath.Base(object.MacOSPkgApp.PackageInstallerFileSource.ValueString())
+	contentFile.SetName(&fileName)
 
+	fileInfo, err := os.Stat(object.MacOSPkgApp.PackageInstallerFileSource.ValueString())
 	if err != nil {
-		return "", err
+		return nil, err
 	}
+	size := fileInfo.Size()
+	contentFile.SetSize(&size)
 
-	return uploadUrl, nil
+	switch object.ApplicationType.ValueString() {
+	case "AndroidLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphAndroidLobAppContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	case "IosLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphIosLobAppContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	case "MacOSDmgApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphMacOSDmgAppContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	case "MacOSLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphMacOSLobAppContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	case "MacOSPkgApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphMacOSPkgAppContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	case "ManagedAndroidLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphManagedAndroidLobAppContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	case "ManagedIOSLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphManagedIOSLobAppContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	case "ManagedMobileLobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphManagedMobileLobAppContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	case "Win32LobApp":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphWin32LobAppContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	case "WindowsAppX":
+		return builder.(*deviceappmanagement.MobileAppsItemGraphWindowsAppXContentVersionsRequestBuilder).
+			ByMobileAppContentId(*contentVersion.GetId()).
+			Files().
+			Post(ctx, contentFile, nil)
+	default:
+		return nil, fmt.Errorf("unsupported application type: %s", object.ApplicationType.ValueString())
+	}
 }
 
 // NewContentVersionsBuilder returns a new content versions builder based on the application type
@@ -107,25 +174,4 @@ func (r *ApplicationsResource) NewContentVersionsBuilder(appType, appID string) 
 	default:
 		return nil, fmt.Errorf("unsupported application type: %s", appType)
 	}
-}
-
-func getFileBytes(object *ApplicationsResourceModel) ([]byte, error) {
-	var filePath string
-
-	switch object.ApplicationType.ValueString() {
-	case "MacOSPkgApp":
-		if object.MacOSPkgApp == nil || object.MacOSPkgApp.PackageInstallerFileSource.IsNull() {
-			return nil, fmt.Errorf("package_installer_file_source is required for MacOSPkgApp")
-		}
-		filePath = object.MacOSPkgApp.PackageInstallerFileSource.ValueString()
-	// TODO: Add other app types here
-	default:
-		return nil, fmt.Errorf("unsupported application type for file upload: %s", object.ApplicationType.ValueString())
-	}
-
-	fileBytes, err := os.ReadFile(filePath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read file %s: %s", filePath, err.Error())
-	}
-	return fileBytes, nil
 }
