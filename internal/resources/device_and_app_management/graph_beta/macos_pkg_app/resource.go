95c9a156c5fbd9c53e6dc467c0150c011b84eeeb
ShocOne <dafydd.watkins@deploymenttheory.com>
2025-02-12 16:16:11 +0000

    fix: for package imports

--- internal/resources/device_and_app_management/graph_beta/macos_pkg_app/resource.go
+++ internal/resources/device_and_app_management/graph_beta/macos_pkg_app/resource.go
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	ResourceName  = "graph_beta_device_and_app_management_applications"
+	ResourceName  = "graph_beta_device_and_app_management_macos_pkg_app"
 	CreateTimeout = 180
 	UpdateTimeout = 180
 	ReadTimeout   = 180
