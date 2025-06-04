87e80905666bb40f3c92777d6e111a670fd944f1
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-06-04 09:54:49 +0100

    fix

--- examples/microsoft365_graph_beta/microsoft365_graph_beta_user_license_assignment/import.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Import using user object ID
-terraform import microsoft365_graph_beta_user_license_assignment.example "12345678-1234-1234-1234-123456789012"
-
-# Import using user principal name (UPN)
-terraform import microsoft365_graph_beta_user_license_assignment.example "john.doe@example.com" 
