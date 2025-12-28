50575f3c6405730e735e4098da4c9c668f2d140b
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-12-28 22:51:09 +0000

    chore: fix up file paths (#1469)
    * chore: pipeline
    
    * remove
    
    * more bug testing
    
    * fix: file paths and script logic

--- internal/provider/ephemeral_resources.go
+++ internal/provider/ephemeral_resources.go
@@ -3,7 +3,7 @@ package provider
 import (
 	"context"
 
-	auditEvents "github.com/deploymenttheory/terraform-provider-microsoft365/internal/services/ephemeral/multitenant_management/audit_events"
+	graphBetaAuditEvents "github.com/deploymenttheory/terraform-provider-microsoft365/internal/services/ephemeral/multitenant_management/graph_beta/audit_events"
 	//windowsAutopilotDeviceCSVImport "github.com/deploymenttheory/terraform-provider-microsoft365/internal/services/ephemeral/windows_autopilot_device_csv_import"
 	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
@@ -15,7 +15,7 @@ var _ provider.ProviderWithEphemeralResources = &M365Provider{}
 // EphemeralResources defines the ephemeral resources implemented in the provider.
 func (p *M365Provider) EphemeralResources(_ context.Context) []func() ephemeral.EphemeralResource {
 	return []func() ephemeral.EphemeralResource{
-		auditEvents.NewAuditEventsEphemeralResource,
+		graphBetaAuditEvents.NewAuditEventsEphemeralResource,
 		//windowsAutopilotDeviceCSVImport.NewWindowsAutopilotDeviceCSVImportEphemeralResource,
 	}
 }
